5950e0eb9f3497c94307123509b05cf43ccf1806
qjd-yyds <qjd1774@163.com>
2022-01-26 22:43:48 +0800

    feat: img api..

--- packages/next-app/components/p_home/Banner.js
+++ packages/next-app/components/p_home/Banner.js
@@ -1,9 +1,26 @@
+import Link from 'next/link';
 import Slider from 'react-slick';
+import s from './banner.module.css';
 const Banner = ({ data = [] }) => {
   console.log(data, 'Banner');
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
   return (
-    <section>
-      <Slider></Slider>
+    <section className={s.warp}>
+      <Slider {...settings}>
+        {data.map((item) => {
+          return (
+            <Link href="course/detail/[id]" as={`course/detail/${item.courseId}`}>
+              <img className={s.image} src={item.img} key={item.courseId} alt={item.title} />
+            </Link>
+          );
+        })}
+      </Slider>
     </section>
   );
 };
